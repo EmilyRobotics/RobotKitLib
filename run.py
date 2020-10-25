72d788af35bd4a909bb3e1a278ff80c5f0b97d61
Eli Thorpe <ethds24@gmail.com>
2020-10-25 16:14:36 -0400

    merge inital networktables

--- run.py
+++ run.py
@@ -4,28 +4,54 @@ import sys
 import robot
 import time
 
+from networktables import NetworkTables
+import threading
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 # robot loop
 """
 
-main init
-
-if telop:
-    telop init
-
-    loop telop until done
+check networktable for updates
 
 if auton:
     auton init
-    loop auton until done
+    repeat auton every 20 miliseconds until not auton
+
+if telop:
+    telop init
+    repeat teleop every 20 miliseconds until not teleop
+    repeatly check for controller values
+
+
 
 """
+ip = "10.10.10.10" #ip address
+
 
 
 class main():
-
-    def __init__(self, mode):
+    def __init__(self):
         self.r = robot.MyRobot()
+        self.connected = False
         
+    #inital connection to networktable, check for updates
+    def connect(self):
+        NetworkTables.initialize(server=ip)
+        NetworkTables.addConnectionListener(self.connectionListener, immediateNotify=True)
+
+        sd = NetworkTables.getTable("TODO")
+        sd.addEntryListener(self.valueChanged)
+
+    
+    def connectionListener(self, connected, info):
+        print(info, "; Connected=%s" % connected)
+        self.connected = True
+
+    #Print the values changed in the network table
+    def valueChanged(self, table, key, value, isNew):
+        print("valueChanged: key: '%s'; value: %s; isNew: %s" % (key, value, isNew))
 
     def start(self):    
         self.r.robotInit()
@@ -42,8 +68,5 @@ class main():
 
 
 if __name__ == "__main__":
-    if sys.argv[1] == "auton":
-        mode = "a"
-    else:
-        mode = None
-    m = main(mode)
+    m = main()
+    m.connect()
