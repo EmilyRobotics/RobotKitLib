601a9d183c597b7600a2b682f00acadab85b9b26
Eli Thorpe <ethds24@gmail.com>
2020-10-26 19:31:08 -0400

    Add illegial buzzer class

--- pikitlib.py
+++ pikitlib.py
@@ -1,5 +1,9 @@
 import Motor
 from networktables import NetworkTables
+import RPi.GPIO as GPIO
+
+
+
 
 class XboxController():
     def __init__(self, id):
@@ -73,6 +77,20 @@ class XboxController():
 
     def getYButtonReleased(self):
         self.getButtonReleased(3)
+
+class IllegialBuzzer():
+    def __init__(self):
+        print("Never use this outside testing")
+        GPIO.setwarnings(False)
+        self.Buzzer_Pin = 17
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.Buzzer_Pin,GPIO.OUT)
+
+    def set(self, value):
+        if value == 0:
+            GPIO.output(self.Buzzer_Pin,False)
+        else:
+            GPIO.output(self.Buzzer_Pin,True)
 
 
 class SpeedController():
